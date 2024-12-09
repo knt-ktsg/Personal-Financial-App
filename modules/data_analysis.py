1e6111bf39774257a75eff23f4fe1759c0de70ed
Kento Kitasuga <hokutonokento0706@gmail.com>
2024-12-09 12:07:25 -0800

    Update a file

--- modules/data_analysis.py
+++ modules/data_analysis.py
@@ -7,6 +7,7 @@ def analyze_spending(df):
         return
 
     print("--- Total Spending by Category ---")
+    df = df[df['Category'] != "Income"]
     total_spending = df.groupby("Category")["Amount"].sum()
     print(total_spending.to_string())
 
@@ -18,6 +19,7 @@ def calculate_monthly_spending(df):
 
     print("--- Average Monthly Spending ---")
     df['Date'] = pd.to_datetime(df['Date'])
+    df = df[df['Category'] != "Income"]
     average_spending = df.groupby(df['Date'].dt.month)['Amount'].mean().round(2)
     average_spending.index.name = None
     print(average_spending.to_string())
@@ -29,6 +31,7 @@ def top_spending(df):
         return
 
     print("--- Top Spending category ---")
+    df = df[df['Category'] != "Income"]
     max_row = df[df.Amount == df.Amount.max()]
     category = max_row['Category'].to_string(index=None).replace(" ", "")
     max_amount = max_row['Amount'].to_string(index=None).replace(" ", "")
